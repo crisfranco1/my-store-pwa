19cd9f6cc46328f2e19dfdd990ca0e43f45cffdc
Cristian David Franco Garcia <cristiandavidfrancogarcia@gmail.com>
2020-06-13 12:54:51 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit{
+
+  constructor(private swUpdate: SwUpdate) { }
+
+  ngOnInit(){
+    this.updatePwa();
+  }
+
+  updatePwa() {
+    this.swUpdate.available.subscribe(value => {
+      console.log('Update PWA: ' + value);
+      window.location.reload();
+    });
+  }
+}
